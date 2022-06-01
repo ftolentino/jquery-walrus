aa2b04130cd9b2444500728c66b8ca438ab60e76
ftolentino <filmer.tolentino@gmail.com>
2022-06-01 13:47:35 -0700

    WIP add light-text when dark button is clicked
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,36 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $("clickable").click(function() {
     $("#walrus-showing").slideToggle();
     $("#walrus-hidden").slideToggle();
   });
 });
 
 $(document).ready(function() {
-  $(".newClickable").click(function() {
-    $(".sand-castles-hidden").slideToggle();
-    $(".sand-castles-show").slideToggle();
+  $("newClickable").click(function() {
+    $("sand-castles-hidden").slideToggle();
+    $("sand-castles-show").slideToggle();
 
   });
 });
 
-$(document).ready(function() {
+$(document).ready(function() {              //Light button
   $("button#light").click(function () {
-    $(".light-text").removeClass();
     $("body").removeClass();
+    $("theme").removeClass();
   });
 });
 
-$(document).ready(function() {
+$(document).ready(function() {              //Dark Button
   $("button#dark").click(function () {
-    $("body").removeClass();
-    $(".dark-text").removeClass()
-    $(".dark-text").addClass("light-text");
+    $("theme").removeClass("container");
     $("body").addClass("bg-dark");
+    $("theme").addClass("light-text");
   });
 });
+
+  $(document).ready(function() {
+    $("h1").click(function () {
+      $("h1").addClass("border-3");
+      $("h1").addClass("bg-info");
+    });
+  });
